7a31b5d04842c9bb466b8e576708179e57afbb73
Ilya Salanovich <ilysalan@WKMZT0696382.global.publicisgroupe.net>
2023-07-04 22:23:12 +0200

    create tests for gendiff

---
+++ __tests__/gendiff-test.js
@@ -0,0 +1,13 @@
+test('flat test', () => {
+    const filepath1 = '/__tests__/file1.json';
+    const filepath2 = '/__tests__/file2.json';
+    const correctResult = `{
+        - follow: false
+          host: hexlet.io
+        - proxy: 123.234.53.22
+        - timeout: 50
+        + timeout: 20
+        + verbose: true
+      }`
+    expect(gendiff(filepath1, filepath2)).toStrictEqual(correctResult);
+});
---
+++ src/gendiff.js
@@ -0,0 +1,8 @@
+import { readFileSync } from 'node:fs';
+
+const gendiff = (path1, path2) => {
+
+}
+
+
+return readFileSync(path);
